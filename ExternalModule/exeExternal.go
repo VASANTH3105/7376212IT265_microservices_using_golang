236e6202e0854df8fc9a7efa0ed41b382d2579cf
VASANTH_3105 <vasanth.it2125@gmail.com>
2024-03-19 19:06:28 +0530

    Imported External Modules from GITHUB imported and used in local module

---
+++ ExternalModule/exeExternal.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/VASANTH3105/math"
+)
+
+func main() {
+	sum := math.Add(1, 2)
+	fmt.Println(sum)
+}
---
+++ ExternalModule/go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.22.1
+
+require github.com/VASANTH3105/math v0.0.0-20240319133245-922a797f9c71
