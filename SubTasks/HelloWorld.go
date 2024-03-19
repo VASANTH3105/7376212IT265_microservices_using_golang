9d161e8aab1d9b05636b26a8879f0f0276969b56
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-03-19 13:56:54 +0530

    First HelloWorld program in go.

---
+++ SubTasks/HelloWorld.go
@@ -0,0 +1,13 @@
+/*@author VASANTH S*/
+//I am gonna try to print my First prg in Golang
+
+package main
+
+// let's import fmt-package to perform formatting and printing output...
+import "fmt"
+
+func main() { 
+	fmt.Println("Hello World") 
+}
+
+//use - or _ industry standards
